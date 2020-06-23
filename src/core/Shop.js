38a4e562b39d6608c32b156b1935450894f64af8
GUO CHENXI <guochenxi@GUOs-MacBook-Pro.local>
2020-06-23 12:02:24 +1000

    push to git

---
+++ src/core/Shop.js
@@ -0,0 +1,100 @@
+import React, {useState, useEffect} from 'react';
+import Layout from './Layout';
+import Card from './Card';
+import {getcategories} from './apiCore';
+import Checkbox from './Checkbox';
+import RadioBox from './RadioBox';
+import {prices} from "./fixedPrices"
+
+const Shop = () => {
+
+    const [myFilters,
+        setMyFilters] = useState({
+        filters: {
+            category: [],
+            price: []
+        }
+    });
+
+    const [categories,
+        setCategories] = useState([])
+    const [error,
+        setError] = useState(false);
+    const init = () => {
+        getcategories().then(data => {
+            if (data.error) {
+                setError(data.error)
+
+            } else {
+                setCategories(data)
+            }
+        });
+    };
+
+    useEffect(() => {
+        init()
+    }, [])
+
+    const handleFilters = (filters, filterBy) => {
+        const newFilters = {
+            ...myFilters
+        }
+        newFilters.filters[filterBy] = filters;
+
+        if(filterBy == "price"){
+            let priceValues = handlePrice(filters);
+            newFilters.filters[filterBy] = priceValues;
+        }
+        loadFilteredResults(myFilters.filters)
+
+        setMyFilters(newFilters);
+    };
+
+    const handlePrice = value => {
+        const data = prices
+        let array = []
+
+        for(let key in data) {
+            if(data[key]._id === parseInt(value)){
+                array = data[key].array
+            }
+        }
+
+        return array;
+    };
+
+    const loadFilteredResults = (newFilters) => {
+        console.log(newFilters)
+    }
+
+    return (
+        <Layout
+            title="Shop Page"
+            description="Search and find accessories"
+            className="container-fluid">
+
+            <div className="row">
+                <div className="col-4">
+                    <h4>Filter by categories</h4>
+                    <ul>
+                        <Checkbox
+                            categories={categories}
+                            handleFilters={filters => handleFilters(filters, "category")}/>
+                    </ul>
+
+                    <h4>Filter by price range</h4>
+                    <div>
+                        <RadioBox
+                            prices={prices}
+                            handleFilters={filters => handleFilters(filters, "price")}/>
+                    </div>
+                </div>
+
+                <div className="col-8">{JSON.stringify(myFilters)}</div>
+            </div>
+
+        </Layout>
+    );
+}
+
+export default Shop;
---
+++ src/core/apiCore.js
@@ -0,0 +1,22 @@
+import {API} from "../config";
+
+export const getProducts = sortby => {
+    return fetch(`${API}/products?sortBy=${sortby}&order=desc&limit=6`, {
+        method: "GET"
+    })
+    .then(response => {
+        return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+
+export const getcategories = () => {
+    return fetch(`${API}/categories`, {
+        method: "GET"
+    })
+    .then(response => {
+        return response.json();
+    })
+    .catch(err => console.log(err));
+};
